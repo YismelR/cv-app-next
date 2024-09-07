ee2f882dfabdb524ab5b52c894c87a21072ef13d
YismelR <yismel_rodriguez@outlook.com>
2024-09-06 22:26:38 -0400

    applied a different svg for the dropdown accordion

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -31,7 +31,20 @@ const AccordionTrigger = React.forwardRef<
             {...props}
         >
             {children}
-            <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+            </svg>
         </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
 ));
